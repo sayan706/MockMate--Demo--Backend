b3ba708667a8983a0a0297b4bbde4f0fd079c870
sayan706 <sayanchatterjee706@gmail.com>
2026-06-17 15:09:27 +0530

    updated by sayan

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import gevent.monkey
 gevent.monkey.patch_all()
 
+import os
 from app import create_app, socketio
 
 flask_app = create_app()
 
 if __name__ == '__main__':
     print("Type of flask_app:", type(flask_app))
+    port = int(os.environ.get('PORT', 5000))
     # allow_unsafe_werkzeug=True is needed for running Flask-SocketIO locally
-    socketio.run(app=flask_app, debug=True, port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app=flask_app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
